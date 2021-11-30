7804ace3a28160257c845c992a5e9f0370c8b1a7
unknown <bhm727@yahoo.com>
2021-11-30 18:17:37 -0500

    Added Post and Put

--- controllers/logController.js
+++ controllers/logController.js
@@ -1,20 +1,49 @@
 const Express = require('express');
 const router = Express.Router();
-const { where } = require('sequelize/location');
+// const { where } = require('sequelize/location');
 const { LogModel } = require('../models');
 
 //* PRACTICE ***
-router.get('/practice', (req, res) => {
-    res.send('Hey, This is the practice route!!')
-});
+// router.get('/practice', (req, res) => {
+//     res.send('Hey, This is the practice route!!')
+// });
 
 //* POST ***
+router.post("/register", async (req, res) => {
 
+    let { what, where, calories, category, date, photo, feeling } = req.body.log;
 
-//* UPDATE ***
-// Update Code from Ben
+    try {
+        const NewLog = await Log.create({
+            what,
+            where,
+            calories,
+            category,
+            date,
+            photo,
+            feeling
+        });
+
+        res.status(201).json({
+            message: "Log successfully created",
+            user: NewLog
+        })
+    } catch (err) {
+        if (err instanceof UniqueConstraintError) {
+            res.status(409).json({
+                message: "Food already listed. Try Updating instead.",
+            });
+        } else {
+            res.status(500).json({
+                message: "Unable to Log Meal",
+            });
+        }
+    }
+});
+
+// Ben - Update
 router.put("/update/:id", async (req, res) => {
-    const {updatedLog} = req.body.log;
+    const {food, location, calorieNumber, mealType, date, photo, feeling} = req.body.log;
     const id = req.params.id;
 
     const query = {
@@ -23,7 +52,7 @@ router.put("/update/:id", async (req, res) => {
             owner: req.user.id
         }
     };
-//Comment
+
     const updatedLog = {
         what: food,
         where: location,

27b62e0408e501f2bb36788cb5afabfa93b41727
unknown <bhm727@yahoo.com>
2021-12-02 12:31:42 -0500

    Delete/Update Works

--- controllers/logController.js
+++ controllers/logController.js
@@ -7,7 +7,7 @@ const { LogModel } = require("../models");
 //* POST ***
 router.post("/create", validateJWT, async (req, res) => {
     const { id } = req.user;
-    
+
     const { what, where, calories, category, date, photo, feelings } = req.body.log;
     const NewLog = {
         what,
@@ -32,7 +32,7 @@ router.post("/create", validateJWT, async (req, res) => {
 router.put("/update/:id", validateJWT, async (req, res) => {
     const { what, where, calories, category, date, photo, feelings } = req.body.log;// **these need to match our request**
     const logId = req.params.id;
-    const {id} = req.user;
+    const { id } = req.user;
     // console.log(id);
     // console.log(req.params, 'req.params');
 
@@ -72,23 +72,26 @@ router.put("/update/:id", validateJWT, async (req, res) => {
 //* DELETE ***
 // ! still needs validate
 router.delete("/delete/:id", validateJWT, async (req, res) => {
-    const ownerId = req.user.id;
     const logId = req.params.id;
-
+    const { id } = req.user;
+    const query = {
+        where: {
+            id: logId,
+            owner: id
+        }
+    };
     try {
-        const query = {
-            where:{
-                id: logId,
-                owner: ownerId
-            }
-        };
-
-        await LogModel.destroy(query);
-        res.status(200).json({ message: 'Log deleted' });
+        const deleteLog = await LogModel.destroy(query);
+        res.status(200).json({
+            message: `${deleteLog} Logs successfully deleted!`,
+            query: query
+        });
     } catch (err) {
         res.status(500).json({ error: err });
+        message = "Error deleting log";
     }
-});
+}
+);
 
 //* GET ***
 router.get('/mine', validateJWT, async (req, res) => {
